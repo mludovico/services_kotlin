bf7ebb160bae87501e5627484de549ae9c28cb33
Marcelo Ludovico de Paula <marcelo.paula@eptv.com.br>
2021-03-05 17:19:23 -0300

    Initial commit

---
+++ app/src/main/java/br/com/mludovico/services/DrinkWaterReminderTask.kt
@@ -0,0 +1,14 @@
+package br.com.mludovico.services
+
+import android.content.Context
+
+class DrinkWaterReminderTask {
+
+    companion object {
+        const val ACTION_INCREMENT_WATER_COUNT = "action-increment-water-count"
+
+        private fun incrementWaterCount(context: Context) {
+            PreferencesUtils
+        }
+    }
+}
